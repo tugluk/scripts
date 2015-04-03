14a0b3415a76fec8f34e0b4b94c12ac2929ce255
Ozan Tugluk <ozantugluk@gmail.com>
2015-04-03 06:51:45 -0400

    Update single.sh

--- single.sh
+++ single.sh
@@ -5,12 +5,12 @@ tlevel=$1
 file=pltosout.bin.$tlevel
 
 echo "Processing" $file
-if [ ! -f ./GENPLIF_$tlevel.DAT ];
+if [ ! -f ./GENPLIF_$tlevel.dat ];
 then
     cp $file  pltosout.bin
     genplif tecplot < genplif.opt &>/dev/null
     surftec < genplif.opt &>/dev/null       
-    mv GENPLIF.DAT field_$tlevel.dat
+    mv GENPLIF.DAT GENPLIF_$tlevel.dat
     mv SURFTEC.DAT surf_$tlevel.dat
 else
     echo "skipping  $file"
